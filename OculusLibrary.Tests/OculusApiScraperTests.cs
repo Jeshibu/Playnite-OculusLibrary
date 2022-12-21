6518475ff431c04d354b1e65c0150457ab39e6f0
Jeshibu <jeshibu@gmail.com>
2022-12-21 15:45:47 +0100

    Added test run

--- OculusLibrary.Tests/OculusApiScraperTests.cs
+++ OculusLibrary.Tests/OculusApiScraperTests.cs
@@ -1,33 +1,33 @@
-﻿using NSubstitute;
-using NUnit.Framework;
-using OculusLibrary.DataExtraction;
+﻿using OculusLibrary.DataExtraction;
 using Playnite.SDK;
 using Playnite.SDK.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace OculusLibrary.Tests
 {
     public class OculusApiScraperTests
     {
-        [Test]
+        [Fact]
         public async Task Asgards_Wrath_Parses_Correctly()
         {
             var subject = Setup("Asgard's Wrath", out var webClient);
             string appId = "1180401875303371";
 
             var data = await subject.GetMetadata(appId);
-            Assert.AreEqual("Asgard's Wrath", data.Name);
+            Assert.Equal("Asgard's Wrath", data.Name);
             Assert.NotNull(data.Description);
-            Assert.AreEqual("1.6.0", data.Version);
+            Assert.Equal("1.6.0", data.Version);
             ReleaseDateEquals(2019, 10, 10, data.ReleaseDate.Value);
-            Assert.AreEqual(appId, data.GameId);
+            Assert.Equal(appId, data.GameId);
             Assert.NotNull(data.BackgroundImage?.Path);
-            Assert.AreEqual(new MetadataNameProperty("Oculus"), data.Source);
+            Assert.Equal(new MetadataNameProperty("Oculus"), data.Source);
             MetadataPropertyCollectionsMatch(data.Features, new[] { "Single Player", "VR", "VR Standing", "VR Seated", "VR Room-Scale", "VR Motion Controllers" });
             MetadataPropertyCollectionsMatch(data.Platforms, new[] { "Oculus Rift", "Oculus Rift S" }, new[] { "pc_windows" });
             MetadataPropertyCollectionsMatch(data.Tags, new[] { "VR Comfort: Moderate" });
@@ -35,23 +35,23 @@ namespace OculusLibrary.Tests
             MetadataPropertyCollectionsMatch(data.Publishers, new[] { "Oculus" });
             MetadataPropertyCollectionsMatch(data.AgeRatings, new[] { "PEGI 18" });
             MetadataPropertyCollectionsMatch(data.Genres, new[] { "Action", "Adventure", "RPG" });
-            Assert.AreEqual(89, data.CommunityScore);
+            Assert.Equal(89, data.CommunityScore);
         }
 
-        [Test]
+        [Fact]
         public async Task Sprint_Vector_Parses_Correctly()
         {
             var subject = Setup("Sprint Vector", out var webClient);
             string appId = "1425858557493354";
 
             var data = await subject.GetMetadata(appId);
-            Assert.AreEqual("Sprint Vector", data.Name);
+            Assert.Equal("Sprint Vector", data.Name);
             Assert.NotNull(data.Description);
-            Assert.AreEqual("0.0.0.111496", data.Version);
+            Assert.Equal("0.0.0.111496", data.Version);
             ReleaseDateEquals(2018, 2, 2, data.ReleaseDate.Value);
-            Assert.AreEqual(appId, data.GameId);
+            Assert.Equal(appId, data.GameId);
             Assert.NotNull(data.BackgroundImage?.Path);
-            Assert.AreEqual(new MetadataNameProperty("Oculus"), data.Source);
+            Assert.Equal(new MetadataNameProperty("Oculus"), data.Source);
             MetadataPropertyCollectionsMatch(data.Features, new[] { "Single Player", "Multiplayer", "VR", "VR Standing", "VR Seated", "VR Room-Scale", "VR Motion Controllers" });
             MetadataPropertyCollectionsMatch(data.Platforms, new[] { "Oculus Rift", "Oculus Rift S" }, new[] { "pc_windows" });
             MetadataPropertyCollectionsMatch(data.Tags, new[] { "VR Comfort: Intense" });
@@ -59,24 +59,25 @@ namespace OculusLibrary.Tests
             MetadataPropertyCollectionsMatch(data.Publishers, new[] { "Survios" });
             MetadataPropertyCollectionsMatch(data.AgeRatings, new[] { "PEGI 3" });
             MetadataPropertyCollectionsMatch(data.Genres, new[] { "Action", "Racing", "Sports" });
-            Assert.AreEqual(82, data.CommunityScore);
+            Assert.Equal(82, data.CommunityScore);
         }
 
-        [TestCase("Asgard's Wrath")]
-        [TestCase("Sprint Vector")]
+        [Theory]
+        [InlineData("Asgard's Wrath")]
+        [InlineData("Sprint Vector")]
         public async Task Names_Parse_Correctly(string gameName)
         {
             var subject = Setup(gameName, out var webClient);
 
             var output = await subject.GetGameName("1234");
-            Assert.AreEqual(gameName, output);
+            Assert.Equal(gameName, output);
         }
 
         private void ReleaseDateEquals(int expectedYear, int expectedMonth, int expectedDay, ReleaseDate actual)
         {
-            Assert.AreEqual(expectedYear, actual.Year);
-            Assert.AreEqual(expectedMonth, actual.Month);
-            Assert.AreEqual(expectedDay, actual.Day);
+            Assert.Equal(expectedYear, actual.Year);
+            Assert.Equal(expectedMonth, actual.Month);
+            Assert.Equal(expectedDay, actual.Day);
         }
 
         private void MetadataPropertyCollectionsMatch(HashSet<MetadataProperty> metadataProperties, string[] namePropertyValues, string[] specPropertyValues = null)
@@ -86,7 +87,7 @@ namespace OculusLibrary.Tests
             expectedStrings.AddRange(specPropertyValues ?? new string[0]);
             string expectedString = string.Join(", ", expectedStrings);
 
-            Assert.AreEqual(namePropertyValues.Length + (specPropertyValues?.Length ?? 0), metadataProperties.Count, "Expected [{0}] but found[{1}]", expectedString, propertyString);
+            Assert.Equal(namePropertyValues.Length + (specPropertyValues?.Length ?? 0), metadataProperties.Count);
             foreach (var nameProp in namePropertyValues)
             {
                 bool contains = metadataProperties.OfType<MetadataNameProperty>().Any(x => x.Name == nameProp);
@@ -108,18 +109,48 @@ namespace OculusLibrary.Tests
 
         private OculusApiScraper Setup(string gameName, out IWebClient webclient)
         {
-            var htmlContent = File.ReadAllText($@"{TestContext.CurrentContext.TestDirectory}\{gameName}.html");
-            var jsonContent = File.ReadAllText($@"{TestContext.CurrentContext.TestDirectory}\{gameName}.json");
+            var htmlContent = File.ReadAllText($@".\{gameName}.html");
+            var jsonContent = File.ReadAllText($@".\{gameName}.json");
 
-            webclient = Substitute.For<IWebClient>();
-            webclient.DownloadString(default).ReturnsForAnyArgs(htmlContent);
-            webclient.DownloadStringAsync(default).ReturnsForAnyArgs(Task.FromResult(htmlContent));
-            webclient.UploadValues(default, default, default).ReturnsForAnyArgs(jsonContent);
-            webclient.UploadValuesAsync(default, default, default).ReturnsForAnyArgs(Task.FromResult(jsonContent));
-
-            var fakeLogger = Substitute.For<ILogger>();
+            webclient = new FakeWebclient(htmlContent, jsonContent);
 
             return new OculusApiScraper(webclient);
+        }
+
+        private class FakeWebclient:IWebClient
+        {
+            public FakeWebclient(string htmlContent, string jsonContent)
+            {
+                HtmlContent = htmlContent;
+                JsonContent = jsonContent;
+            }
+
+            public string HtmlContent { get; }
+            public string JsonContent { get; }
+
+            public void Dispose()
+            {
+            }
+
+            public string DownloadString(string address)
+            {
+                return HtmlContent;
+            }
+
+            public async Task<string> DownloadStringAsync(string address)
+            {
+                return HtmlContent;
+            }
+
+            public string UploadValues(string address, string method, NameValueCollection data)
+            {
+                return JsonContent;
+            }
+
+            public async Task<string> UploadValuesAsync(string address, string method, NameValueCollection data)
+            {
+                return JsonContent;
+            }
         }
     }
 }
--- OculusLibrary.Tests/PathNormaliserTests.cs
+++ OculusLibrary.Tests/PathNormaliserTests.cs
@@ -1,44 +1,49 @@
-﻿using NSubstitute;
-using NUnit.Framework;
-using OculusLibrary.OS;
+﻿using OculusLibrary.OS;
 using System.Collections.Generic;
+using Xunit;
 
 namespace OculusLibrary.Tests
 {
     public class PathNormaliserTests
     {
-        [Test]
+        [Fact]
         public void Get_Drive_Letter_From_DeviceId()
         {
             var path = @"\\?\Volume{a0bf4e34-c90f-4005-815c-9a5a485e40ad}\Oculus\Software";
 
-            var fakeWMOProvider = Substitute.For<IWMODriveQueryProvider>();
-            fakeWMOProvider.GetDriveData().Returns(new List<WMODrive> {
-                new WMODrive {
-                    DeviceId = @"\\?\Volume{DCBDB210-C414-409E-B108-C2BFA7395E1F}\",
-                    DriveLetter = "X:"
-                },
-                new WMODrive {
-                    DeviceId = @"\\?\Volume{DCBDB210-C414-409E-B108-C2BFA7395E1F}\",
-                    DriveLetter = ""
-                },
-                new WMODrive { 
-                    DeviceId = @"\\?\Volume{a0bf4e34-c90f-4005-815c-9a5a485e40ad}\",
-                    DriveLetter = "D:"
-                },
-                new WMODrive {
-                    DeviceId = @"\\?\Volume{ECBDB210-D414-509E-B108-C2BFA7395E1F}\",
-                    DriveLetter = "C:"
-                },
-            });
+            var fakeWMOProvider = new FakeWMOProvider();
 
             using (var subject = new PathNormaliser(fakeWMOProvider))
             {
                 var normalisedPath = subject.Normalise(path);
-                Assert.AreEqual(@"D:\Oculus\Software", normalisedPath);
+                Assert.Equal(@"D:\Oculus\Software", normalisedPath);
             }
+        }
 
-                
+        private class FakeWMOProvider : IWMODriveQueryProvider
+        {
+            public List<WMODrive> GetDriveData()
+            {
+                return new List<WMODrive>
+                {
+                    new WMODrive {
+                        DeviceId = @"\\?\Volume{DCBDB210-C414-409E-B108-C2BFA7395E1F}\",
+                        DriveLetter = "X:"
+                    },
+                    new WMODrive {
+                        DeviceId = @"\\?\Volume{DCBDB210-C414-409E-B108-C2BFA7395E1F}\",
+                        DriveLetter = ""
+                    },
+                    new WMODrive {
+                        DeviceId = @"\\?\Volume{a0bf4e34-c90f-4005-815c-9a5a485e40ad}\",
+                        DriveLetter = "D:"
+                    },
+                    new WMODrive {
+                        DeviceId = @"\\?\Volume{ECBDB210-D414-509E-B108-C2BFA7395E1F}\",
+                        DriveLetter = "C:"
+                    },
+                };
+            }
         }
     }
 }
